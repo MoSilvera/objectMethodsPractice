3bfd7f2339e4a8116a056f0f7e03f49fef6e06b8
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-05 11:41:10 -0600

    completed challenge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-//refrence to the place in the dom where the information will be printed 
+//refrence to the place in the dom where the information will be printed
 const carContainer = document.getElementById("carContainer")
 //function that injects HTML elments into the DOM
 const printToTheCarContainer = (whatToPrint) => {carContainer.innerHTML += (whatToPrint)}
@@ -7,7 +7,7 @@ const nameElement = (sale) => { return `<h2>${sale.sales_agent.first_name} ${sal
 //returns the element for the car info key value pairs
 const carInfoElement = (keyOrValue) => { return `<div>${keyOrValue[0]}: ${keyOrValue[1]}`}
 //returns the element for the profit dollar amount
-const profitElement = (sale) => { return `<h3>Gross Profit:$ ${sale.gross_profit}</h3>`}
+const profitElement = (sale) => { return `<h3>Gross Profit:$${sale.gross_profit}</h3>`}
 
 //iterates through the sales array
 let weeklySalesReport = () => salesByWeek.forEach(sale => {
@@ -25,4 +25,26 @@ let weeklySalesReport = () => salesByWeek.forEach(sale => {
 
 weeklySalesReport()
 
+const searchInput = document.getElementById("searchInput")
+const searchResults = document.getElementById("searchResults")
+
+
+searchInput.addEventListener('keypress', event => {
+   if (event.keyCode === 13) {
+      searchResults.innerHTML= ""
+     const searchTerm = event.target.value
+     salesByWeek.forEach(sale => {
+        let agent = sale.sales_agent
+        for(const value of Object.values(agent)) {
+           if(value.toUpperCase().includes(searchTerm.toUpperCase())){
+            searchResults.innerHTML += `
+            <h2>${agent.first_name} ${agent.last_name}</h2>
+            <div>email: ${agent.email}</div>
+            <div>sale: $${sale.gross_profit}</div `
+           }}
+
+     })
+
+   }
+ })
 
