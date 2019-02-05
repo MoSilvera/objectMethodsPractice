bf088380830f3943cd5dbf44570e6fad46403c2e
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-05 08:52:58 -0600

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+//refrence to the place in the dom where the information will be printed 
+const carContainer = document.getElementById("carContainer")
+//function that injects HTML elments into the DOM
+const printToTheCarContainer = (whatToPrint) => {carContainer.innerHTML += (whatToPrint)}
+//returns the element for the seller name
+const nameElement = (sale) => { return `<h2>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</h2>`}
+//returns the element for the car info key value pairs
+const carInfoElement = (keyOrValue) => { return `<div>${keyOrValue[0]}: ${keyOrValue[1]}`}
+//returns the element for the profit dollar amount
+const profitElement = (sale) => { return `<h3>Gross Profit:$ ${sale.gross_profit}</h3>`}
+
+//iterates through the sales array
+let weeklySalesReport = () => salesByWeek.forEach(sale => {
+   //prints the name element to the car container
+  printToTheCarContainer(nameElement(sale))
+  //turns the value of sales.vehicle -an object- into an array fully of inner arrays contianing key value pairs 
+   const carSubArray = Object.entries(sale.vehicle)
+   //for each interior array, the key value pairs are printed to the DOM
+    for (const entry of carSubArray) {
+      printToTheCarContainer(carInfoElement(entry))
+    }
+    //prints the profit element to the car container
+   printToTheCarContainer(profitElement(sale))
+})
+
+weeklySalesReport()
+
+
